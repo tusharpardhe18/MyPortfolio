03fe237310322f6ff322972c2284998a5e7b3099
Tushar Pardhe <tusharpardhe2018@gmail.com>
2025-07-08 14:04:12 +0530

    loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,9 +1,24 @@
-import { Html, useProgress } from "@react-three/drei"
+import { Html, useProgress } from "@react-three/drei";
+import "./loader.css";
 
 const Loader = () => {
-    const { progress } = useProgress();
+  const { progress } = useProgress();
+
   return (
-    <Html center className="text-xl font-normal text-center" >{progress}% loaded</Html>
+    <Html center>
+      <div className="flex flex-col items-center justify-center">
+        <div className="container">
+          <div className="box box-1"></div>
+          <div className="box box-2"></div>
+          <div className="box box-3"></div>
+          <div className="box box-4"></div>
+          <div className="box box-5"></div>
+        </div>
+        <p className="text-white mt-4 text-sm">
+          {Math.floor(progress)}% loaded
+        </p>
+      </div>
+    </Html>
   );
 };
 
