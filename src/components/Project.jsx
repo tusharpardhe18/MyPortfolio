ea7297a0dceecded17299d24d71386dcfbbaed26
Tushar Pardhe <tusharpardhe2018@gmail.com>
2025-07-17 21:12:52 +0530

    Mobile Image Preview Bug

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,38 +1,52 @@
-import { useState } from 'react'
-import ProjectDetails from './ProjectDetails'
+import { useState } from "react";
+import ProjectDetails from "./ProjectDetails";
 
-const Project = ({title, description, subDescription, href, image, tags, setPreview}) => {
-    const [isHidden, setIsHidden] = useState(false);
+const Project = ({
+  title,
+  description,
+  subDescription,
+  href,
+  image,
+  tags,
+  setPreview,
+}) => {
+  const [isHidden, setIsHidden] = useState(false);
   return (
     <>
-        <div 
-            className="flex-wrap items-center justify-between py-10  space-y-14 sm:flex sm:space-y-0" 
-            onMouseEnter={() => {setPreview(image)}}
-            onMouseLeave={() => {setPreview(null)}}
-        >
-            <p className='text-2xl'>{title}</p>
-            <div className='flex gap-5 mt-2 text-sand'>
-                {tags.map((tag) => (
-                    <span key={tag.id}>{tag.name}</span>
-                ))}
-            </div>
-            <button onClick={() => setIsHidden(true)} className='flex items-center gap-1 cursor-none hover-animation'>
-                Read More
-                <img src="assets/arrow-right.svg" className='w-5' />
-            </button>
+      <div
+        className="flex-wrap items-center justify-between py-10 space-y-7 sm:flex sm:space-y-0"
+        onMouseEnter={() => setPreview(image)}
+        onMouseLeave={() => setPreview(null)}
+        onClick={() => setPreview(null)}
+      >
+        <p className="text-2xl">{title}</p>
+        <div className="flex flex-wrap gap-2 mt-2 text-sand break-words max-w-full">
+          {tags.map((tag) => (
+            <span key={tag.id}>{tag.name}</span>
+          ))}
         </div>
-        <div className="bg-gradient-to-r from-transparent via-neutral-700 to-transparent h-[1px] w-full" />
-        { isHidden && (<ProjectDetails 
-            title={title}
-            description={description}
-            subDescription={subDescription}
-            image={image}
-            tags={tags}
-            href={href}
-            closeModal={() => setIsHidden(false)}
-        />)}
+        <button
+          onClick={() => setIsHidden(true)}
+          className="flex items-center gap-1 cursor-none hover-animation"
+        >
+          Read More
+          <img src="assets/arrow-right.svg" className="w-5" />
+        </button>
+      </div>
+      <div className="bg-gradient-to-r from-transparent via-neutral-700 to-transparent h-[1px] w-full" />
+      {isHidden && (
+        <ProjectDetails
+          title={title}
+          description={description}
+          subDescription={subDescription}
+          image={image}
+          tags={tags}
+          href={href}
+          closeModal={() => setIsHidden(false)}
+        />
+      )}
     </>
-  )
-}
+  );
+};
 
-export default Project
+export default Project;
